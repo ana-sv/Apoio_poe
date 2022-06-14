ab171b9dec9a8b936a3b9b00ba2236fb2cb0a614
anarita-sv <anarita.svideira@gmail.com>
2022-06-14 22:43:23 +0100

    começei a atribuição automatica

--- Apoio_poe/src/pt/isec/pa/apoio_poe/model/fsm/states/AtribuicaoPropostas.java
+++ Apoio_poe/src/pt/isec/pa/apoio_poe/model/fsm/states/AtribuicaoPropostas.java
@@ -1,47 +1,77 @@
 package pt.isec.pa.apoio_poe.model.fsm.states;
 
+import pt.isec.pa.apoio_poe.model.data.Candidatura;
 import pt.isec.pa.apoio_poe.model.data.Data;
+import pt.isec.pa.apoio_poe.model.data.Proposta.TipoProposta;
 import pt.isec.pa.apoio_poe.model.fsm.Context;
 import pt.isec.pa.apoio_poe.model.fsm.StateAdapter;
 
-class AtribuicaoPropostas extends StateAdapter{
+class AtribuicaoPropostas extends StateAdapter {
     private Integer index = 2;
 
-
- AtribuicaoPropostas(Context context, Data data) {
+    AtribuicaoPropostas(Context context, Data data) {
         super(context, data);
     }
 
-    public boolean atribuicaoAuto(){
-         // se fase aberta --> atribui automaticamente 
-                // se existem empates   changeState(ApoioPoeState.RESOLVE_EMPATES);
-                // se nao exitem    continua no mesmo estado
-                
-            return false; 
+    public boolean atribuicaoAuto() {
+
+        if (data.getSituacaoEstados(index) == true) { // se esta fase está aberta , entao começa a atribuir
+
+            for (Candidatura a : data.getListaCandidaturas().values()) {
+
+                if (!data.alunoExisteListaFinal(a.getNumEstudante())) { // se o aluno ainda nao está na lista final
+                                                                        // definitiva
+
+                    if ( a.getListaPropostas().contains(TipoProposta.T3)) {
+                        data.getListaFinal().put(a.getNumEstudante(), a.getCodigoProposta(0));
+                         // a lista de propostas já é um array com codigos das propostas
+                         data.getListaCandidaturas().remove(a.getNumEstudante());  
+                    }
+
+                }  else {
+
+
+                     // SE o proximo aluno tem a mesma primeira opção que outros na lista 
+                      // e se for estágio ve se tem nota para tal , se não tiver nota testa a segunda opção 
+
+                     // adiciona-nos na lista de empates 
+                     // vai ao estado desempatar e volta?
+
+                     // SE nao , atribui a primeira opção 
+
+
+
+                }
+
+
+     
+
+
+            }
+
+        }
+
+        return false; // "Estado Fechado, nao pode fazer nada disto
 
     }
 
-    public String filtraListasAlunos( Integer q ,String filtros ) {
+    public String filtraListasAlunos(Integer q, String filtros) {
         // TO DO
         return " nao implementado ainda";
     }
 
-  
-    public String filtraListasPropostas( Integer q ,String filtros ) {
+    public String filtraListasPropostas(Integer q, String filtros) {
         // TO DO
         return " nao implementado ainda";
     }
 
-
-    public String mostraListas() {   // MUDAR PARA DADOS??
+    public String mostraListas() { // MUDAR PARA DADOS??
         StringBuilder s = new StringBuilder();
 
         s.append(data.infoCandidaturasToString(false));
 
-    return s.toString();
+        return s.toString();
     }
-
-
 
     @Override
     public State getState() {
@@ -56,14 +86,15 @@ class AtribuicaoPropostas extends StateAdapter{
     @Override
     public boolean fechaEstado() {
 
+        // REVER
 
-        // Apenas será possível fechar esta fase se todos os alunos com candidaturas submetidas possuírem projeto atribuído
-        if( data.getListaCandidaturas().containsValue(null)){
-            return false; 
-        }
-        else{
+        // Apenas será possível fechar esta fase se todos os alunos com candidaturas
+        // submetidas possuírem projeto atribuído
+        if (data.getListaCandidaturas().containsValue(null)) {
+            return false;
+        } else {
             changeState(State.ATRIBUICAO_ORIENTADORES);
-            return true; 
+            return true;
         }
     }
 
@@ -73,16 +104,13 @@ class AtribuicaoPropostas extends StateAdapter{
             changeState(State.OPCOES_CANDIDATURAS);
             return true;
         }
-        return false; 
+        return false;
 
     }
 
-
     @Override
-    public boolean getSituacaoEstado(){
+    public boolean getSituacaoEstado() {
         return data.getSituacaoEstados(this.index);
     }
 
-
-    
 }
